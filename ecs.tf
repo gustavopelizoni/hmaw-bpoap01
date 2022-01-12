272dbf440102a1b955269d0812c57643eb1fb968
gustavo.pelizoni <aws@bild.com.br>
2022-01-12 16:33:14 -0300

    chave

--- ecs.tf
+++ ecs.tf
@@ -4,10 +4,10 @@ module "ecs-hmaw-bpoap01" {
   VPC_ID         = "vpc-0d51056d1c87cd1c2"
   CLUSTER_NAME   = "hmaw-bpoap01"
   INSTANCE_TYPE  = "t3a.small"
-  SSH_KEY_NAME   = "hmaw-bpoap01.pem"
+  SSH_KEY_NAME   = "hmaw-bpoap01"
   VPC_SUBNETS    = "subnet-0751b3e3cc871be8a,subnet-068b41b0ef91dc341"
   ENABLE_SSH     = true
-  SSH_SG         = "sgecsdvawsysap01"
+  SSH_SG         = "sgecshmawbpoap01"
   LOG_GROUP      = "log-hmaw-bpoap01"
   AWS_ACCOUNT_ID = "667460020190"
   AWS_REGION     = "us-east-1"
