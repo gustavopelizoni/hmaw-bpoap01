9971bc70dd60739fe2b0003aa105cfa1a7e6f9a7
gustavo.pelizoni <aws@bild.com.br>
2022-01-12 18:35:14 -0300

    aws_security_group_rule

--- modules/ecs-cluster/securitygroups.tf
+++ modules/ecs-cluster/securitygroups.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "cluster" {
 
 
 resource "aws_security_group_rule" "cluster-ingress" {
-  count                    = var.ENABLE_SSH ? 1 : 0
+  count                    = lenth(var.ENABLE_SSH ? 1 : 0)
   security_group_id        = aws_security_group.cluster.id
   type                     = "ingress"
   from_port                = 22
