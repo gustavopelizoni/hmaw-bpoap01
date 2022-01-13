9d9c6948438d4eeb864ffe40d0964b7a6d258253
gustavo.pelizoni <aws@bild.com.br>
2022-01-13 14:25:33 -0300

    ecr

--- modules/ecs-service/ecs-service.tf
+++ modules/ecs-service/ecs-service.tf
@@ -6,6 +6,10 @@ resource "aws_ecr_repository" "bpo-service-hml" {
   name = var.APPLICATION_NAME_BPO_SERVICE
 }
 
+resource "aws_ecr_repository" "bpo-beat-celery-hml" {
+  name = var.APPLICATION_NAME_BPO_BEAT_CELERY
+}
+
 #
 # get latest active revision
 #
