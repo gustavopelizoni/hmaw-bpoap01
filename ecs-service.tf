18445e480f074f990ec985478c0196b640f64fa4
gustavo.pelizoni <aws@bild.com.br>
2022-01-13 11:17:05 -0300

    bpo-service-hml

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,5 +1,5 @@
 ## ECS Service
-module "hmaw-bpoap01" {
+module "bpo-service-hml" {
   source = "github.com/gustavopelizoni/hmaw-bpoap01//modules/ecs-service"
   VPC_ID = "vpc-0d51056d1c87cd1c2"
   #APPLICATION_NAME    = "hmaw-bpoap01"
